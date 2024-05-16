0ddd6dd24c1e4891ced623b1a3c83252ee4adb34
Travis Ramos <travis.ramos5@gmail.com>
2024-05-16 17:34:23 -0500

    added dashboard page and moved login

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,19 @@
 "use client";
 
-import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
+import { useRouter } from "next/navigation";
 
 export default function App() {
-	const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+	const router = useRouter();
 
-	function listTodos() {
-		client.models.Todo.observeQuery().subscribe({
-			next: (data) => setTodos([...data.items]),
-		});
-	}
-
-	function deleteTodo(id: string) {
-		client.models.Todo.delete({ id });
-	}
-
-	useEffect(() => {
-		listTodos();
-	}, []);
-
-	function createTodo() {
-		client.models.Todo.create({
-			content: window.prompt("Todo content"),
-		});
-	}
+	const handleLoginClick = () => {
+		router.push("/dashboard");
+	};
 
 	return (
-		<Authenticator>
-			{({ signOut, user }) => (
-				<main>
-					<h1>{user?.signInDetails?.loginId}'s todos</h1>
-					<h1>My todos</h1>
-					<button onClick={createTodo}>+ new</button>
-					<ul>
-						{todos.map((todo) => (
-							<li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-								{todo.content}
-							</li>
-						))}
-					</ul>
-					<div>
-						🥳 App successfully hosted. Try creating a new todo.
-						<br />
-						<a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-							Review next steps of this tutorial.
-						</a>
-					</div>
-					<button onClick={signOut}>Sign out</button>
-				</main>
-			)}
-		</Authenticator>
+		<main>
+			<h1 className="text-3xl font-bold">Welcome</h1>
+			<button onClick={handleLoginClick}>Login</button>
+		</main>
 	);
 }
