c5a5d1308cc95cef3abfee2318aedf062cafbd87
Travis Ramos <travis.ramos5@gmail.com>
2024-05-16 18:00:22 -0500

    added simple landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,130 @@
 "use client";
 
 import "./../app/app.css";
-import { useRouter } from "next/navigation";
+import { Dialog } from "@headlessui/react";
+import { useState } from "react";
 
 export default function App() {
-	const router = useRouter();
-
-	const handleLoginClick = () => {
-		router.push("/dashboard");
-	};
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 	return (
-		<main>
-			<h1 className="text-3xl font-bold">Welcome</h1>
-			<button onClick={handleLoginClick}>Login</button>
+		<main className="bg-white">
+			<header className="absolute inset-x-0 top-0 z-50">
+				<nav
+					className="flex items-center justify-between p-6 lg:px-8"
+					aria-label="Global"
+				>
+					<div className="flex lg:flex-1">
+						<a href="#" className="-m-1.5 p-1.5">
+							<span className="">Spot Me</span>
+						</a>
+					</div>
+					<div className="flex lg:hidden">
+						<button
+							type="button"
+							className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+							onClick={() => setMobileMenuOpen(true)}
+						>
+							<span className="sr-only">Open main menu</span>
+							{/* <Bars3Icon className="h-6 w-6" aria-hidden="true" /> */}
+						</button>
+					</div>
+					<div className="hidden lg:flex lg:flex-1 lg:justify-end">
+						<a
+							href="/dashboard"
+							className="text-sm font-semibold leading-6 text-gray-900"
+						>
+							Log in <span aria-hidden="true">&rarr;</span>
+						</a>
+					</div>
+				</nav>
+				<Dialog
+					className="lg:hidden"
+					open={mobileMenuOpen}
+					onClose={setMobileMenuOpen}
+				>
+					<div className="fixed inset-0 z-50" />
+					<Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+						<div className="flex items-center justify-between">
+							<a href="#" className="-m-1.5 p-1.5">
+								<span className="">Spot Me</span>
+							</a>
+							<button
+								type="button"
+								className="-m-2.5 rounded-md p-2.5 text-gray-700"
+								onClick={() => setMobileMenuOpen(false)}
+							>
+								<span className="sr-only">Close menu</span>
+								{/* <XMarkIcon className="h-6 w-6" aria-hidden="true" /> */}
+							</button>
+						</div>
+						<div className="mt-6 flow-root">
+							<div className="-my-6 divide-y divide-gray-500/10">
+								<div className="py-6">
+									<a
+										href="/dashboard"
+										className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+									>
+										Log in
+									</a>
+								</div>
+							</div>
+						</div>
+					</Dialog.Panel>
+				</Dialog>
+			</header>
+
+			<div className="relative isolate px-6 pt-14 lg:px-8">
+				<div
+					className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+					aria-hidden="true"
+				>
+					<div
+						className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+						style={{
+							clipPath:
+								"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+						}}
+					/>
+				</div>
+				<div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+					<div className="hidden sm:mb-8 sm:flex sm:justify-center">
+						<div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+							Where you go to manage your money.
+						</div>
+					</div>
+					<div className="text-center">
+						<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+							Personal Finance Tracking Made Easy
+						</h1>
+						<p className="mt-6 text-lg leading-8 text-gray-600">
+							Budgeting and tracking your expenses has never been easier. With
+							Spot Me, you can easily manage your finances and get a clear view
+							of your spending habits.
+						</p>
+						<div className="mt-10 flex items-center justify-center gap-x-6">
+							<a
+								href="/dashboard"
+								className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							>
+								Get started
+							</a>
+						</div>
+					</div>
+				</div>
+				<div
+					className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+					aria-hidden="true"
+				>
+					<div
+						className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+						style={{
+							clipPath:
+								"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+						}}
+					/>
+				</div>
+			</div>
 		</main>
 	);
 }
