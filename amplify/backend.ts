0019c4f60aa0eb058a65436157ba8a3a2ed11686
Travis Ramos <travis.ramos5@gmail.com>
2024-05-22 14:18:08 -0500

    removing define auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,8 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource.js";
+// import { auth } from "./auth/resource.js";
 import { data } from "./data/resource.js";
 
 defineBackend({
-	auth,
+	// auth,
 	data,
 });
