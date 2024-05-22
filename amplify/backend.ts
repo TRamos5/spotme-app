32f583f4ff1167fcc6d10e430626c38bfcfe076a
Travis Ramos <travis.ramos5@gmail.com>
2024-05-22 14:01:55 -0500

    testing change

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,8 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource.js";
 import { data } from "./data/resource.js";
-import { UserPool } from "aws-cdk-lib/aws-cognito";
-import { RemovalPolicy } from "aws-cdk-lib";
 
-const backend = defineBackend({
+defineBackend({
 	auth,
 	data,
 });
-
-const userPool = backend.auth.resources.userPool as UserPool;
-userPool.applyRemovalPolicy(RemovalPolicy.DESTROY);
