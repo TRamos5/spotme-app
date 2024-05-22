5a2ba24d50bc12508c75e08e82d4eaa522ddeae2
Travis Ramos <travis.ramos5@gmail.com>
2024-05-22 14:22:27 -0500

    adding back for last test

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,14 +4,14 @@ import { defineAuth } from "@aws-amplify/backend";
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-// export const auth = defineAuth({
-// 	loginWith: {
-// 		email: true,
-// 	},
-// 	userAttributes: {
-// 		preferredUsername: {
-// 			mutable: true,
-// 			required: true,
-// 		},
-// 	},
-// });
+export const auth = defineAuth({
+	loginWith: {
+		email: true,
+	},
+	userAttributes: {
+		preferredUsername: {
+			mutable: true,
+			required: true,
+		},
+	},
+});
